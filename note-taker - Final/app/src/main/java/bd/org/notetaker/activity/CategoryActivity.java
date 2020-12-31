a647d77dfa89e63267c59950dd9ad399e4fa3073
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-31 10:33:28 +0600

    Update CategoryActivity.java

--- note-taker - Final/app/src/main/java/bd/org/notetaker/activity/CategoryActivity.java
+++ note-taker - Final/app/src/main/java/bd/org/notetaker/activity/CategoryActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
 import bd.org.notetaker.R;
 import bd.org.notetaker.db.OpenHelper;
 import bd.org.notetaker.fragment.CategoryFragment;
@@ -19,38 +18,31 @@ public class CategoryActivity extends AppCompatActivity implements RecyclerFragm
 	public static final int RESULT_CHANGE = 101;
 	private Toolbar toolbar;
 	private CategoryFragment fragment;
-
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		setTheme(Category.getStyle(getIntent().getIntExtra(OpenHelper.COLUMN_THEME, Category.THEME_GREEN)));
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_category);
-
 		toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-
 		try {
 			//noinspection ConstantConditions
 			getSupportActionBar().setDisplayShowTitleEnabled(false);
 		} catch (Exception ignored) {
 		}
-
 		toolbar.findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				onBackPressed();
 			}
 		});
-
 		if (savedInstanceState == null) {
 			fragment = new CategoryFragment();
-
 			getSupportFragmentManager().beginTransaction()
 				.add(R.id.container, fragment)
 				.commit();
 		}
 	}
-
 	@Override
 	public void onChangeSelection(boolean state) {
 		if (state) {
@@ -65,23 +57,19 @@ public class CategoryActivity extends AppCompatActivity implements RecyclerFragm
 				.animate(R.anim.fade_in);
 		}
 	}
-
 	@Override
 	public void toggleOneSelection(boolean state) {
 	}
-
 	@Override
 	public void onBackPressed() {
 		if (fragment.isFabOpen) {
 			fragment.toggleFab(true);
 			return;
 		}
-
 		if (fragment.selectionState) {
 			fragment.toggleSelection(false);
 			return;
 		}
-
 		Intent data = new Intent();
 		data.putExtra("position", fragment.categoryPosition);
 		data.putExtra(OpenHelper.COLUMN_COUNTER, fragment.items.size());
