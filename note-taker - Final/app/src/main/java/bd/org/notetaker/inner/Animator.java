ac7efde23e5d4c0ff410e8134e91854d210d14c6
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-01 14:11:20 +0600

    Update Animator.java

--- note-taker - Final/app/src/main/java/bd/org/notetaker/inner/Animator.java
+++ note-taker - Final/app/src/main/java/bd/org/notetaker/inner/Animator.java
@@ -7,7 +7,6 @@ import android.view.animation.AnimationUtils;
 
 @SuppressWarnings("unused")
 public class Animator {
-
 	private Context context;
 	private View view;
 	private boolean clear = true;
@@ -15,45 +14,36 @@ public class Animator {
 	private int start_visiblity = View.VISIBLE;
 	private int end_visiblity = View.VISIBLE;
 	private AnimatorListener listener = null;
-
 	private Animator(Context context) {
 		this.context = context;
 	}
-
 	public static Animator create(Context context) {
 		return new Animator(context);
 	}
-
 	public <T extends View> Animator on(T view) {
 		this.view = view;
 		return this;
 	}
-
 	public Animator setDelay(long delay) {
 		this.delay = delay;
 		return this;
 	}
-
 	public Animator setClear(boolean clear) {
 		this.clear = clear;
 		return this;
 	}
-
 	public Animator setStartVisibility(int visiblity) {
 		this.start_visiblity = visiblity;
 		return this;
 	}
-
 	public Animator setEndVisibility(int visiblity) {
 		this.end_visiblity = visiblity;
 		return this;
 	}
-
 	public Animator setListener(AnimatorListener listener) {
 		this.listener = listener;
 		return this;
 	}
-
 	public void animate(int anim_id) {
 		Animation animation = AnimationUtils.loadAnimation(context, anim_id);
 		if (delay > 0) {
@@ -61,10 +51,8 @@ public class Animator {
 		}
 		animation.setAnimationListener(new Animation.AnimationListener() {
 			boolean end_status = false;
-
 			@Override
 			public void onAnimationStart(Animation animation) {}
-
 			@Override
 			public void onAnimationEnd(Animation animation) {
 				if (!end_status) {
@@ -76,16 +64,13 @@ public class Animator {
 					}
 				}
 			}
-
 			@Override
 			public void onAnimationRepeat(Animation animation) {}
 		});
 		view.setVisibility(start_visiblity);
 		view.startAnimation(animation);
 	}
-
 	public interface AnimatorListener {
 		void onAnimate();
 	}
-
 }
