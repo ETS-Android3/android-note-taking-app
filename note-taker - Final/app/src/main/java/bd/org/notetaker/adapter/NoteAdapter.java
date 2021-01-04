36577758db8af255bb787326cfcff269c1aae02f
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-04 11:56:06 +0600

    Update NoteAdapter.java

--- note-taker - Final/app/src/main/java/bd/org/notetaker/adapter/NoteAdapter.java
+++ note-taker - Final/app/src/main/java/bd/org/notetaker/adapter/NoteAdapter.java
@@ -2,9 +2,7 @@ package bd.org.notetaker.adapter;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-
 import java.util.ArrayList;
-
 import bd.org.notetaker.R;
 import bd.org.notetaker.adapter.template.ModelAdapter;
 import bd.org.notetaker.model.Note;
@@ -14,7 +12,6 @@ public class NoteAdapter extends ModelAdapter<Note, NoteViewHolder> {
 	public NoteAdapter(ArrayList<Note> items, ArrayList<Note> selected, ClickListener<Note> listener) {
 		super(items, selected, listener);
 	}
-
 	@Override
 	public NoteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		return new NoteViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note, parent, false));
