364f817aaa67fb73f8161b2839cfa45953becc18
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2021-01-05 09:51:50 +0600

    Update CategoryAdapter.java

--- note-taker - Final/app/src/main/java/bd/org/notetaker/adapter/CategoryAdapter.java
+++ note-taker - Final/app/src/main/java/bd/org/notetaker/adapter/CategoryAdapter.java
@@ -2,9 +2,7 @@ package bd.org.notetaker.adapter;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-
 import java.util.ArrayList;
-
 import bd.org.notetaker.R;
 import bd.org.notetaker.adapter.template.ModelAdapter;
 import bd.org.notetaker.model.Category;
@@ -14,7 +12,6 @@ public class CategoryAdapter extends ModelAdapter<Category, CategoryViewHolder> 
 	public CategoryAdapter(ArrayList<Category> items, ArrayList<Category> selected, ClickListener<Category> listener) {
 		super(items, selected, listener);
 	}
-
 	@Override
 	public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		return new CategoryViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_category, parent, false));
